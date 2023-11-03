0e2c23471c07378285b251051a4d17000ba90e1c
infinith4 <infinith4@gmail.com>
2023-11-03 10:25:56 +0900

    add dashboard

---
+++ quickstart/nextjs_typescript_tailwind_app/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+// `app/dashboard/page.tsx` is the UI for the `/dashboard` URL
+
+import { Metadata } from 'next'
+ 
+export const metadata: Metadata = {
+  title: 'Next.jsaaa',
+}
+
+export default function Page() {
+  return <p>Hello, Dashboard Page!</p>
+}
